e8dfbd06b12b97b00a7101f74860109b53c3c2b2
drolejoel <usansky@yahoo.com>
2014-04-27 05:11:32 -0400

    finished code added to template
    FINALLY I managed to figure this out.  That was a TOUGH exercise.  Next
    step will be to add all the comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,47 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setmean <- function(inverse) inv <<- inverse
+        getmean <- function() inv
+        list(set = set, 
+             get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getmean()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)   # this line solves the matrix
+        x$setmean(inv)
+        inv
 }
+
+# ==========================================
+
+# test both functions
+
+x <- matrix(runif(36), nrow=6)           # create a matrix
+solve(x)                                         # solve it
+
+e <- makeCacheMatrix(x)
+cacheSolve(e)                                    # first time calculates the matrix
+cacheSolve(e)                                    # second time retrieves from cache
+
+# compare the speed
+system.time( cacheSolve(e) )                     # first time calculates the matrix
+system.time( cacheSolve(e) )                     # second time retrieves from cache
+
